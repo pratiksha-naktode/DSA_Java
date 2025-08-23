0a749b4c7815000ea0b035cb84df672eed31cb3c
Pratiksha Naktode <naktodepratiksha@gmail.com>
2025-08-23 22:20:27 +0530

    pairsum

--- Array/ArrayPair.java
+++ Array/ArrayPair.java
@@ -1,5 +1,5 @@
 public class ArrayPair{
-    public static void ArrayPair(int arr[]){
+    public static void ArrayPair2(int arr[]){
         int count=0;
         for(int i=0;i<arr.length;i++){
             for(int j=i+1;j<arr.length;j++){
@@ -11,10 +11,31 @@ public class ArrayPair{
         System.out.println("total pairs in array"+count);
     }
 
+// find Sum of Pair equal to target and return that pairs
+public static void PairSum(int arr[],int target){//BRUET FORCE METHOD O(N)^3
+    boolean found=false;
+    int sum=0;
+    for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+            sum=arr[i];
+            sum+=arr[j];
+            if(sum==target){
+                found=true;
+                System.out.println("["+arr[i]+" ,"+arr[j]+"]");
+            }
+            sum=0;
+        }
+      
+    }
+    if(!found){
+        System.out.println("Not Found");
+    }
+}
     
     public static void main(String args[]){
-        int arr[]={10,20,30,40,50};
-        ArrayPair(arr);
+        int arr[]={10,20,30,40};
+        ArrayPair2(arr);
+        PairSum(arr, 50);
         
     }
 }
