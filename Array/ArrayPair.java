3654c223eac8bf344b8e3102d0ad670f362d19da
Pratiksha Naktode <naktodepratiksha@gmail.com>
2025-08-23 23:32:25 +0530

    finished

--- Array/ArrayPair.java
+++ Array/ArrayPair.java
@@ -19,7 +19,7 @@ public static void PairSum(int arr[],int target){//BRUET FORCE METHOD O(N)^3
             for(int j=i+1;j<arr.length;j++){
             sum=arr[i];
             sum+=arr[j];
-            if(sum==target){
+            if(sum==target){ //(arr[i]+arr[j])
                 found=true;
                 System.out.println("["+arr[i]+" ,"+arr[j]+"]");
             }
@@ -31,11 +31,36 @@ public static void PairSum(int arr[],int target){//BRUET FORCE METHOD O(N)^3
         System.out.println("Not Found");
     }
 }
+
+public static void SumPair(int arr[],int target){
+    int start=0;
+    int count=0;
+    int end=arr.length-1;
+    while(start<end){
+        int sum=arr[start]+arr[end];
+        if(sum>target)
+        {
+            end--;
+        }
+        else  if(sum<target)
+        {
+            start++;
+        }
+        else{
+            count++;
+            System.out.println("["+arr[start]+","+arr[end]+" ]");
+        start++;
+        end--;
+        }
+    }
+    System.out.println(count+"pairs exist with targeted sum");
+}
     
     public static void main(String args[]){
-        int arr[]={10,20,30,40};
+        int arr[]={10,20,30,40,50,60};
         ArrayPair2(arr);
-        PairSum(arr, 50);
+       PairSum(arr, 60);
+        SumPair(arr, 60);
         
     }
 }
