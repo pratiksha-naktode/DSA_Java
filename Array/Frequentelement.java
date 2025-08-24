cd01340e4fa46a8ddb4de4784fc0211716853ee5
Pratiksha Naktode <naktodepratiksha@gmail.com>
2025-08-24 21:53:12 +0530

    done

--- Array/Frequentelement.java
+++ Array/Frequentelement.java
@@ -1,3 +1,22 @@
-public class Frequentelement{
-    
+public class FrequentElement{
+    public static void main(String[] args) {
+        int arr[]={1,5,6,6,2,3,4,4,};
+        int maxcount=0;
+        int ans=arr[0];
+        for(int i=0;i<arr.length;i++){
+            int count=0;
+          if((arr[i]%2)==0){
+              for(int j=i+1;j<arr.length;j++){
+                    if(arr[i]==arr[j]){
+                        count++;
+                    }
+            }
+            if(maxcount<count){
+                maxcount=count;
+                ans=arr[i];
+            }
+          }
+        }
+        System.out.println("frequent element "+ans);
+    }
 }
