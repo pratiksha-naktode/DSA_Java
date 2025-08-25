c188778f8bfd0e4d58d74ca8e242a77f21b870b8
Pratiksha Naktode <naktodepratiksha@gmail.com>
2025-08-25 20:41:15 +0530

    MaxSumSizeK

--- Array/MaxSumSizeK.java
+++ Array/MaxSumSizeK.java
@@ -4,11 +4,23 @@
 public class MaxSumSizeK{
 
     public static void main(String[] args) {
-        int arr[]={3,8,1,2,3,4,5};
+        int arr[]={1,3,7,2,11,13};
         int k=3;
+        int maxsum=0;
         for(int i=0;i<k;i++){
-            int maxsum=arr[i];
+            maxsum+=arr[i];
         }
-        
+        int currentsum=maxsum;
+
+        for(int i=k;i<arr.length;i++){
+            currentsum=currentsum-arr[i-k]+arr[i];
+            if(maxsum<currentsum){
+                maxsum=currentsum;
+            }
+        }
+
+        System.out.println(maxsum);
+       //  System.out.println(maxsum);
+
     }
 }
