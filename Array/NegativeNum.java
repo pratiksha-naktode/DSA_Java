bbbfa62da8ee50646b2c7e74b464aa1b54d868ab
Pratiksha Naktode <naktodepratiksha@gmail.com>
2025-08-26 07:18:46 +0530

    first negative

--- Array/NegativeNum.java
+++ Array/NegativeNum.java
@@ -1,13 +1,15 @@
 public class NegativeNum{
     public static void main(String args[]){
-        int arr[]={-8,7,4,-4,3,8,-1,5,7,-2,7,3};
-        for (int i = 0; i < 10; i++) {
-            if(i%3==0){
-                if(arr[i]<0){
-                    System.out.print(arr[i]+" ");
-                }
+        int arr[]={-1,2,-11,0,3,-1,-1,3,-8,8,-9};
+        int k=3;
+        int start=0;
+        int end=k-1;
+        while(start<end &&start<=arr.length-k){
+            if(arr[start]<0){
+                System.out.print(arr[start]+" ");
             }
-            
+            start++;
+            end++;
         }
     }
 }
