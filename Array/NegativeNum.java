1a6b53ba9165005f5548c58ee5a10a74aa4bc139
Pratiksha Naktode <naktodepratiksha@gmail.com>
2025-08-26 06:40:02 +0530

    first negative

--- Array/NegativeNum.java
+++ Array/NegativeNum.java
@@ -1,5 +1,13 @@
 public class NegativeNum{
     public static void main(String args[]){
-        
+        int arr[]={-8,7,4,-4,3,8,-1,5,7,-2,7,3};
+        for (int i = 0; i < 10; i++) {
+            if(i%3==0){
+                if(arr[i]<0){
+                    System.out.print(arr[i]+" ");
+                }
+            }
+            
+        }
     }
 }
