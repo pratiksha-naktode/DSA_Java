b4da1775e17116b056ab82833ae5b920d64421a9
Pratiksha Naktode <naktodepratiksha@gmail.com>
2025-08-28 09:35:22 +0530

    done

--- Array/Containermostwater.java
+++ Array/Containermostwater.java
@@ -21,9 +21,40 @@ public class Containermostwater{
     }
     return maxwater;
     }
+
+
+    public static int PoniterMethod(int arr[]){
+        int left=0 , maxwater=0 ;
+        int right=arr.length-1;
+
+        while(left<right){
+          int  width=right-left;
+                int min_height=0;
+                if(arr[left]<arr[right]){
+                    min_height=arr[left];
+                }
+                else{
+                    min_height=arr[right];
+                }
+            int currentarea=width*min_height;
+             if(maxwater<currentarea){
+                maxwater=currentarea;
+            }
+            if(arr[left]<arr[right])
+            {
+                left++;
+            }
+            else{
+                right--;
+            }
+
+        }
+        return maxwater;
+    }
     public static void main(String[] args) {
-        int arr[]={1,8,6,2,5,4,8,3,7};
-        int result=BruetForce(arr);
+        int arr[]={1,8,6,2,5,4,8,3,7,9};
+        // int result=BruetForce(arr);
+        int result=PoniterMethod(arr);
         System.out.println("max water "+result);
     }
 }
